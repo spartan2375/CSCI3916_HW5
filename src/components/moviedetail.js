7c531cbd1d26a32443ca6b6dd6c690912179f69b
Xavier S <spartan9077@gmail.com>
2022-04-18 01:31:01 -0600

    added form for input on detail page

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -3,9 +3,35 @@ import { fetchMovie } from "../actions/movieActions";
 import { connect } from "react-redux";
 import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
 import { BsStarFill } from "react-icons/bs";
-import { Image } from "react-bootstrap";
+import { Form, Button, Image } from "react-bootstrap";
 
 class MovieDetail extends Component {
+  constructor(props) {
+    super(props);
+    // this.updateDetails = this.updateDetails.bind(this);
+    // this.login = this.login.bind(this);
+
+    this.state = {
+      details: {
+        comment: "0",
+        rating: "1",
+      },
+    };
+  }
+
+  updateDetails(event) {
+    let updateDetails = Object.assign({}, this.state.details);
+
+    updateDetails[event.target.id] = event.target.value;
+    this.setState({
+      details: updateDetails,
+    });
+
+    const { dispatch } = this.props;
+    console.log(this.state.details);
+    // dispatch(submitLogin(this.state.details));
+  }
+
   componentDidMount() {
     const { dispatch } = this.props;
     if (this.props.selectedMovie == null) {
@@ -54,6 +80,27 @@ class MovieDetail extends Component {
                 &nbsp; <BsStarFill /> {review.rating}
               </p>
             ))}
+
+            <Form className="form-horizontal">
+              <Form.Group controlId="username">
+                <Form.Label>Review</Form.Label>
+                <Form.Control
+                  value={this.state.details.username}
+                  type="email"
+                  placeholder="Enter review"
+                />
+              </Form.Group>
+
+              <Form.Group controlId="password">
+                <Form.Label>Rating</Form.Label>
+                <Form.Control
+                  value={this.state.details.password}
+                  type="text"
+                  placeholder="Rating"
+                />
+              </Form.Group>
+              <Button onClick={this.updateDetails}>Submit</Button>
+            </Form>
           </Card.Body>
         </Card>
       );
