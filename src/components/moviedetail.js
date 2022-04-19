989e29ff6d046dac8b5a78bde551c7692161e3ce
Xavier S <spartan9077@gmail.com>
2022-04-19 15:36:59 -0600

    everything except actually pushing the review work

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -8,28 +8,29 @@ import { Form, Button, Image } from "react-bootstrap";
 class MovieDetail extends Component {
   constructor(props) {
     super(props);
-    // this.updateDetails = this.updateDetails.bind(this);
+    this.updateReview = this.updateReview.bind(this);
     // this.login = this.login.bind(this);
 
     this.state = {
       details: {
-        comment: "0",
-        rating: "1",
+        comment: "Enter review here!",
+        rating: "5",
       },
     };
   }
+  updateReview(event) {
+    let updateReview = Object.assign({}, this.state.details);
 
-  updateDetails(event) {
-    let updateDetails = Object.assign({}, this.state.details);
-
-    updateDetails[event.target.id] = event.target.value;
+    updateReview[event.target.id] = event.target.value;
     this.setState({
-      details: updateDetails,
+      details: updateReview,
     });
+  }
 
-    const { dispatch } = this.props;
-    console.log(this.state.details);
-    // dispatch(submitLogin(this.state.details));
+  submitReview() {
+    alert(
+      "Lets pretend react is easy to learn and a review was added to the db"
+    );
   }
 
   componentDidMount() {
@@ -44,68 +45,67 @@ class MovieDetail extends Component {
       if (!this.props.selectedMovie[0]) {
         return <div>Loading....</div>;
       }
-
-      return (
-        <Card>
-          <Card.Header>Movie Detail</Card.Header>
-          <Card.Body>
-            <Image
-              className="image"
-              src={this.props.selectedMovie[0].image}
-              width={500}
-              thumbnail
-            />
-          </Card.Body>
-          <ListGroup>
-            <ListGroupItem>{this.props.selectedMovie[0].title}</ListGroupItem>
-            <ListGroupItem>
-              <h4>Actors</h4>
-              {this.props.selectedMovie[0].actors.map((actor, i) => (
-                <p key={i}>
-                  <b>{actor.actorName}</b> {actor.characterName}
-                </p>
-              ))}
-            </ListGroupItem>
-            <ListGroupItem>
-              <h4>
-                <BsStarFill />{" "}
-                {this.props.selectedMovie[0].avgRating.toFixed(1)}
-              </h4>
-            </ListGroupItem>
-          </ListGroup>
-          <Card.Body>
-            {this.props.selectedMovie[0].reviews.map((review, i) => (
-              <p key={i}>
-                <b>{getName(review)}</b>&nbsp; {review.comment}
-                &nbsp; <BsStarFill /> {review.rating}
-              </p>
-            ))}
-
-            <Form className="form-horizontal">
-              <Form.Group controlId="username">
-                <Form.Label>Review</Form.Label>
-                <Form.Control
-                  value={this.state.details.username}
-                  type="email"
-                  placeholder="Enter review"
-                />
-              </Form.Group>
-
-              <Form.Group controlId="password">
-                <Form.Label>Rating</Form.Label>
-                <Form.Control
-                  value={this.state.details.password}
-                  type="text"
-                  placeholder="Rating"
-                />
-              </Form.Group>
-              <Button onClick={this.updateDetails}>Submit</Button>
-            </Form>
-          </Card.Body>
-        </Card>
-      );
     };
 
+    return (
+      <Card>
+        <Card.Header>Movie Detail</Card.Header>
+        <Card.Body>
+          <Image
+            className="image"
+            src={this.props.selectedMovie.image}
+            width={500}
+            thumbnail
+          />
+        </Card.Body>
+        <ListGroup>
+          <ListGroupItem>{this.props.selectedMovie.title}</ListGroupItem>
+          <ListGroupItem>
+            <h4>Actors</h4>
+            {this.props.selectedMovie.actors?.map((actor, i) => (
+              <p key={i}>
+                <b>{actor.actorName}</b> {actor.characterName}
+              </p>
+            ))}
+          </ListGroupItem>
+          <ListGroupItem>
+            <h4>
+              <BsStarFill /> {this.props.selectedMovie.avgRating.toFixed(1)}
+            </h4>
+          </ListGroupItem>
+        </ListGroup>
+        <Card.Body>
+          {this.props.selectedMovie.reviews.map((review, i) => (
+            <p key={i}>
+              <b>{getName(review)}</b>&nbsp; {review.comment}
+              &nbsp; <BsStarFill /> {review.rating}
+            </p>
+          ))}
+        </Card.Body>
+        <Form className="form-horizontal">
+          <Form.Group controlId="comment">
+            <Form.Label>Review</Form.Label>
+            <Form.Control
+              onChange={this.updateReview}
+              value={this.state.details.comment}
+              type="text"
+              placeholder="Enter Review Here"
+            />
+          </Form.Group>
+
+          <Form.Group controlId="rating">
+            <Form.Label>Rating</Form.Label>
+            <Form.Control
+              onChange={this.updateReview}
+              value={this.state.details.rating}
+              type="text"
+              placeholder="Enter Rating Here"
+            />
+          </Form.Group>
+          <Button onClick={this.submitReview}>Submit</Button>
+        </Form>
+      </Card>
+    );
     return <DetailInfo />;
   }
 }
